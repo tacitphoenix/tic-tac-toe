9e54b29b87966cbc9904e618bda6c094d8df682c
tacitphoenix <sondiek@gmail.com>
2019-08-08 13:20:34 -0400

    refactor in a row checks from win conditions to be used in game engine

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,5 @@
 module TicTacToe
+    # board conversions
     def self.board
         Array.new(3){Array.new(3, " ")}
     end
@@ -12,6 +13,7 @@ module TicTacToe
         [a[0..2],a[3..5],a[6..8]]
     end
 
+    # is the board valid
     def self.valid_board(str)
         chk_length = str.length == 9
         chk_chars = !str.match(/[^xo ]/)
@@ -29,32 +31,51 @@ module TicTacToe
         (xcnt - ocnt).abs < 2
     end
 
+    # game engine
+    def self.server_move(str)
+        board = to_board(str)
+
+    end
+
+    # win conditions
     def self.winner(str)
         row_win(str) || col_win(str) || dia_win(str)
     end
 
     def self.row_win(str)
-        board = to_board(str)
-        board.each do |row|
-            in_a_row = [row[0], row[1], row[2]]
-            return true if in_a_row.uniq.size < 2
-        end
+        rows(str).each{|row| return true if row.uniq.size < 2}
         return false
     end
 
-    def self.col_win(str)
-        board = to_board(str)
-        (0..2).to_a.each do |col|
-            in_a_row = [board[0][col], board[1][col], board[2][col]]
-            return true if in_a_row.uniq.size < 2
+    def self.rows(str)
+        [].tap do |arr|
+            board = to_board(str)
+            board.each{|row| arr << [row[0], row[1], row[2]]}
         end
-        false
+    end
+
+    def self.col_win(str)
+        cols(str).each{|col| return true if col.uniq.size < 2}
+        return false
+    end
+
+    def self.cols(str)
+        [].tap do |arr|
+            board = to_board(str)
+            (0..2).to_a.each{|col| [board[0][col], board[1][col], board[2][col]]}
+        end
     end
 
     def self.dia_win(str)
+        dias(str).each{|dia| return true if dia.uniq.size < 2}
+        return false
+    end
+
+    def self.dias(str)
         board = to_board(str)
-        ldia = [board[0][0], board[1][1], board[2][2]].uniq.size < 2
-        rdia = [board[2][0], board[1][1], board[0][2]].uniq.size < 2
-        ldia || rdia
+        [
+            [board[0][0], board[1][1], board[2][2]],
+            [board[2][0], board[1][1], board[0][2]]
+        ]
     end
 end
