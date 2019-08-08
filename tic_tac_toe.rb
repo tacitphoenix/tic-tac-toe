64707e8dd0a6d9ec63ebf568b21b00ccdb2a4f1d
tacitphoenix <sondiek@gmail.com>
2019-08-08 11:02:29 -0400

    interface board to string converstion

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,2 +1,17 @@
 module TicTacToe
+    def self.board
+        Array.new(3){Array.new(3, " ")}
+    end
+
+    def self.to_string(board)
+        board.flatten.join()
+    end
+
+    def self.to_board(str)
+        a = str.split("")
+        [a[0..2],a[3..5],a[6..8]]
+    end
+
+    def self.valid_board(board)
+    end
 end
