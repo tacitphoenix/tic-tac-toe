89b08fce9ef73ad1c54da87c11d0a95ed40f118a
tacitphoenix <sondiek@gmail.com>
2019-08-08 11:37:17 -0400

    ensure board is valid

--- test_tic_tac_toe.rb
+++ test_tic_tac_toe.rb
@@ -4,6 +4,9 @@ require_relative "tic_tac_toe"
 class TestTicTacToe < Minitest::Test
   def setup
     @board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    @board_string = "         "
+    @valid_string =  "o x o   x"
+    @invalid_string =  "bad o   x"
   end
 
   def test_new_board
@@ -11,14 +14,18 @@ class TestTicTacToe < Minitest::Test
   end
 
   def test_board_to_string_valid
-    assert_equal TicTacToe.to_string(@board), "         "
+    assert_equal TicTacToe.to_string(@board), @board_string
   end
 
-  def test_board_to_string_not_valid
-    refute_equal TicTacToe.to_string(@board), "X        "
+  def test_board_to_string_invalid
+    refute_equal TicTacToe.to_string(@board), @valid_string
   end
 
   def test_string_to_board
-    assert_equal TicTacToe.to_board("         "), @board
+    assert_equal TicTacToe.to_board(@board_string), @board
+  end
+
+  def test_invalid_board
+    refute TicTacToe.valid_board(@invalid_string)
   end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,6 +12,13 @@ module TicTacToe
         [a[0..2],a[3..5],a[6..8]]
     end
 
-    def self.valid_board(board)
+    def self.valid_board(str)
+        chk_length = str.length == 9
+        chk_chars = !str.match(/[^xo ]/)
+        if chk_length && chk_chars
+            true
+        else
+            false
+        end
     end
 end
